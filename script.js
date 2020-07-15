37afcdd3253c713a4d72db564c07cf77c1240005
ayion <ayorg360@gmail.com>
2020-07-15 21:59:00 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("overlay").classList.add("overlay");
+    document.getElementById("header").classList.add("blur");
+    document.body.classList.add("no-scroll");
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("overlay").classList.remove("overlay");
+    document.getElementById("header").classList.remove("blur");
+    document.body.classList.remove("no-scroll");
+}
+
+function disclaimer() {
+    document.getElementById("disclaimer").classList.add("d-block");
+}
+
+function loader() {
+    load = setTimeout(page, 2000);
+}
+
+function page() {
+    document.getElementById("loader").remove();
+    document.getElementById("website").classList.remove("d-none");
+    document.getElementById("website").classList.add("d-block");
+}
